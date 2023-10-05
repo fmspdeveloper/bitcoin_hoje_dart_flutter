a015c2b7684721c971fd12c42c7a7d7d33a0efee
felip <fmdeveloper2@gmail.com>
2023-10-05 15:17:33 -0300

    atualizando botoes para usar os metodos abaixo para atualizar, salvar e deletar um valor da api. _post _put _patch _delete

--- lib/dados.dart
+++ lib/dados.dart
@@ -13,9 +13,10 @@ class paginaDados extends StatefulWidget {
 }
 
 class _paginaDadosState extends State<paginaDados> {
+  String url = "https://jsonplaceholder.typicode.com";
+
   Future<List<Post>> _carregarDados() async {
-    String url = "https://jsonplaceholder.typicode.com/posts";
-    Uri uri = Uri.parse(url);
+    Uri uri = Uri.parse(url + "/posts");
 
     http.Response response = await http.get(uri);
     if (response.statusCode == 200) {
@@ -43,36 +44,149 @@ class _paginaDadosState extends State<paginaDados> {
     }
   }
 
+  _post() async {
+    var corpo = jsonEncode(
+      {
+        "userId": 122,
+        "id": null,
+        "title": "Titulo novo",
+        "body": "Descricao da postagem",
+      },
+    );
+    Uri uri = Uri.parse(url + "/posts");
+    http.Response response = await http.post(
+      uri,
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+      body: corpo,
+    );
+    print("resposta: ${response.statusCode}");
+    print("resposta: ${response.body}");
+  }
+
+  _put() async {
+    var corpo = jsonEncode(
+      {
+        "userId": 122,
+        "id": null,
+        "title": "Titulo novo",
+        "body": "Descricao da postagem",
+      },
+    );
+    Uri uri = Uri.parse(url + "/posts/2");
+    http.Response response = await http.put(
+      uri,
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+      body: corpo,
+    );
+    print("resposta: ${response.statusCode}");
+    print("resposta: ${response.body}");
+  }
+
+  _patch() async {
+    var corpo = jsonEncode(
+      {
+        "userId": 122,
+        "id": null,
+        "title": "Titulo novo",
+        "body": "Descricao da postagem",
+      },
+    );
+    Uri uri = Uri.parse(url + "/posts/2");
+    http.Response response = await http.patch(
+      uri,
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+      body: corpo,
+    );
+    print("resposta: ${response.statusCode}");
+    print("resposta: ${response.body}");
+  }
+
+  _delete() async {
+    var corpo = jsonEncode(
+      {
+        "userId": 122,
+        "id": null,
+        "title": "Titulo novo",
+        "body": "Descricao da postagem",
+      },
+    );
+    Uri uri = Uri.parse(url + "/posts/2");
+    http.Response response = await http.delete(
+      uri,
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+      body: corpo,
+    );
+    print("resposta: ${response.statusCode}");
+    print("resposta: ${response.body}");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Meus posts"),
       ),
-      body: FutureBuilder<List<Post>>(
-          future: _carregarDados(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            } else if (snapshot.hasError) {
-              return Text("Erro: ${snapshot.error}");
-            } else if (snapshot.hasData) {
-              List<Post> posts = snapshot.data!;
-              return ListView.builder(
-                itemCount: posts.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(posts[index].title),
-                    subtitle: Text(posts[index].body),
-                  );
-                },
-              );
-            } else {
-              return Text("Nenhum dado disponivel");
-            }
-          }),
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: _post,
+                  child: Text("Salvar"),
+                ),
+                ElevatedButton(
+                  onPressed: _put,
+                  child: Text("Atualizar"),
+                ),
+                ElevatedButton(
+                  onPressed: _delete,
+                  child: Text("Remover"),
+                ),
+              ],
+            ),
+            //Future
+
+            Expanded(
+              child: FutureBuilder<List<Post>>(
+                  future: _carregarDados(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    } else if (snapshot.hasError) {
+                      return Text("Erro: ${snapshot.error}");
+                    } else if (snapshot.hasData) {
+                      List<Post> posts = snapshot.data!;
+                      return ListView.builder(
+                        itemCount: posts.length,
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            title: Text(posts[index].title),
+                            subtitle: Text(posts[index].body),
+                          );
+                        },
+                      );
+                    } else {
+                      return Text("Nenhum dado disponivel");
+                    }
+                  }),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
